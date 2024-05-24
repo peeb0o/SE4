f0d25f924613a5eba557d0f71d101a177d53b39a
peter <peter@localpc>
2024-05-24 18:20:57 +0100

    [SE4] Add logic for variable usage to rectangle command class using new method and minor refactor

--- WindowsFormsApp1/Commands/RectangleCommand.cs
+++ WindowsFormsApp1/Commands/RectangleCommand.cs
@@ -1,4 +1,5 @@
-﻿using SE4.Variables;
+﻿using SE4.Exceptions;
+using SE4.Variables;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -21,16 +22,51 @@ namespace SE4
         public override void Execute(ShapeFactory shapeFactory, string[] parameters)
         {
             graphics = shapeFactory.drawPanel.CreateGraphics();
+
+            //Check parameters are exactly 2 
+            if (parameters.Length != 2)
+            {
+                throw new InvalidParameterCountException("Invalid number of parameters");
+            }
+
             if (parameters.Length == 2)
             {
-                string[] coordinates = parameters[1].Split(',');
+                //Split dimensions on the comma
+                string[] dimensions = parameters[1].Split(',');
 
-                if (coordinates.Length == 2 && int.TryParse(coordinates[0], out int width) && int.TryParse(coordinates[1], out int height))
+                //Check correct number of dimensions
+                if (dimensions.Length != 2)
                 {
-                    Rectangle rect = new Rectangle(shapeFactory.penColor, shapeFactory.penX - (width /2), shapeFactory.penY - (height /2), width, height, shapeFactory.fill);
-                    shapeFactory.AddShape(rect);
+                    throw new InvalidParameterCountException("Invalid number of dimensions passed");
                 }
+
+                //Call method to check if variable or literal
+                int width = GetDimensionValue(dimensions[0]);
+                int height = GetDimensionValue(dimensions[1]);
+
+                //Draw rectangle
+                Rectangle rect = new Rectangle(shapeFactory.penColor, shapeFactory.penX - (width /2), shapeFactory.penY - (height /2), width, height, shapeFactory.fill);
+                shapeFactory.AddShape(rect);
+                
             }
+        }
+
+        private int GetDimensionValue(string dimension)
+        {
+            //Check if variable
+            if (variableManager.VariableExists(dimension))
+            {
+                return variableManager.GetVariableValue(dimension);
+            }
+
+            //Otherwise tryparse int
+            if (int.TryParse(dimension, out int value))
+            {
+                return value;
+            }
+
+            //Throw exception in case invalid dimension passed
+            throw new CommandException($"Invalid dimension value: {dimension}");
         }
     }
 }

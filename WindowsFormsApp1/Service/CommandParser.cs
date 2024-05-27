1ee84231a649c13a8bebf4f6d6294e6461f3eadf
peter <peter@localpc>
2024-05-27 11:44:33 +0100

    [SE4] Refactor of commandparser, add logic for loop and if commands

--- WindowsFormsApp1/Service/CommandParser.cs
+++ WindowsFormsApp1/Service/CommandParser.cs
@@ -24,6 +24,14 @@ namespace SE4
         private StoredProgram storedProgram;
         private VariableManager variableManager;
         private ArithmeticOperatorHandler operatorHandler;
+        private ComparisonOperatorHandler comparisonHandler;
+        private EqualsOperatorHandler equalsHandler;
+        private List<string> loopCommands;
+        private string loopCondition;
+        private bool isInLoop = false;
+        private List<string> ifCommands;
+        private bool isInIf = false;
+        private string ifCondition;
 
         public CommandParser(ShapeFactory factory, VariableManager variableManager)
         {
@@ -37,6 +45,10 @@ namespace SE4
             fillCommand = new FillCommand();
             storedProgram = new StoredProgram(variableManager);
             operatorHandler = new ArithmeticOperatorHandler(variableManager, shapeFactory);
+            comparisonHandler = new ComparisonOperatorHandler(variableManager);
+            equalsHandler = new EqualsOperatorHandler(variableManager);
+            this.loopCommands = new List<string>();
+            this.ifCommands = new List<string>();
         }
 
         public void ParseCommand(string command)
@@ -47,12 +59,55 @@ namespace SE4
             //Will parse command based on the first part that is read 
             string commandType = parts[0].ToLower().Trim();
 
+
+            if (isInLoop)
+            {
+                if (command.Trim().ToLower() == "endloop")
+                {
+                    // Execute the loop
+                    ExecuteLoop();
+                    isInLoop = false;
+                    return;
+                }
+                else
+                {
+                    // Collect loop commands
+                    loopCommands.Add(command);
+                }
+                
+            }
+
+            if (isInIf)
+            {
+                if(command.Trim().ToLower() == "endif")
+                {
+                    //execute if block
+                    /*ExecuteIf();*/
+                    isInIf = false;
+
+                    if (EvaluateCondition(ifCondition))
+                    {
+                        foreach (var cmd in ifCommands)
+                        {
+                            this.ParseCommand(cmd);
+                        }
+                    }
+                }
+                else
+                {
+                    //Collect if block commands
+                    ifCommands.Add(command);
+                }
+                return;
+            }
+
             //parse commands using if statements
 
+            //Check if command is trying an operation
             try
             {
 
-                if (command.Contains('+') || command.Contains('-') && command.Contains("="))
+                if (command.ToLower().Contains('+') || command.ToLower().Contains('-')) //&& command.Contains("=")
                 {
                     try
                     {
@@ -67,35 +122,34 @@ namespace SE4
                     }
                 }
 
-
-                /* if (commandType == "var" && !commandType.Contains("="))
-                 {
-                     storedProgram.Execute(shapeFactory, parts);
-                 }*/
-
-
-                /*
-                 * if commandtype == loop
-                 *      figure out num of iterations through loop
-                 *      int x = num of iterations 
-                 *      string[] array = commands to be looped
-                 *      
-                 *     for number of iterations {
-                 *     
-                 *      foreach (string command in commands)
+                if (command.ToLower().Contains("==") || command.ToLower().Contains("!=")) 
                 {
-                    if (!string.IsNullOrWhiteSpace(command))
+                    try
                     {
-                        this.ParseCommand(command);
-                        Console.WriteLine("Parser called"); // for debugging purposes - remove later
+                        if (equalsHandler.TryHandleEqualsOperator(command))
+                        {
+                            return;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
                     }
                 }
 
-                }
-                 */
-
+                //Check command type, default allows for variables to be declared without var keyword
                 switch (commandType)
                 {
+                    case "loop":
+                        loopCondition = command.Substring(command.IndexOf(' ') + 1).Trim();
+                        isInLoop = true;
+                        loopCommands.Clear();
+                        break;
+                    case "if":
+                        ifCondition = command.Substring(command.IndexOf(' ') + 1).Trim();
+                        isInIf = true;
+                        ifCommands.Clear();
+                        break;
                     case "var":
                         storedProgram.Execute(shapeFactory, parts);
                         break;
@@ -125,37 +179,7 @@ namespace SE4
                         shapeFactory.Reset();
                         break;
                     default:
-                        if (command.Contains("="))
-                        {
-                            // Split the command into variable name and value
-                            string[] assignment = command.Split('=');
-                            string variableName = assignment[0].Trim().ToLower();
-                            string valueString = assignment[1].Trim();
-
-                            //Validating variable name using utility class regex pattern
-                            if (!VariableValidation.IsValidVariableName(variableName))
-                            {
-                                PanelUtilities.WriteToPanel(shapeFactory.drawPanel, "Invalid variable name given: " + variableName);
-                                break;
-                            }
-
-                            // Parse the value string to determine the variable value
-                            if (int.TryParse(valueString, out int variableValue))
-                            {
-                                // Set the variable value using the VariableManager
-                                variableManager.UpdateVariable(variableName, variableValue);
-                            }
-                            else
-                            {
-                                // Invalid value string, write to panel
-                                PanelUtilities.WriteToPanel(shapeFactory.drawPanel, "Invalid variable value: " + valueString);
-                            }
-                        }
-                        else
-                        {
-                            // Invalid command, write to panel
-                            throw new InvalidParameterCountException("Invalid number of parameters in command");
-                        }
+                        HandleDefaultCase(command);
                         break;
                 }
             }
@@ -164,6 +188,80 @@ namespace SE4
                 PanelUtilities.WriteToPanel(shapeFactory.drawPanel, ex.Message);
                 throw;
             }
+        }
+
+        private void HandleDefaultCase(string command)
+        {
+            if (command.Contains("="))
+            {
+                // Split the command into variable name and value
+                string[] assignment = command.Split('=');
+                string variableName = assignment[0].Trim().ToLower();
+                string valueString = assignment[1].Trim();
+
+                //Validating variable name using utility class regex pattern
+                if (!VariableValidation.IsValidVariableName(variableName))
+                {
+                    PanelUtilities.WriteToPanel(shapeFactory.drawPanel, "Invalid variable name given: " + variableName);
+                    return;
+                    //break leave to test;
+                }
+
+                // Parse the value string to determine the variable value
+                if (int.TryParse(valueString, out int variableValue))
+                {
+                    // Set the variable value using the VariableManager
+                    variableManager.UpdateVariable(variableName, variableValue);
+                }
+                else
+                {
+                    // Invalid value string, write to panel
+                    PanelUtilities.WriteToPanel(shapeFactory.drawPanel, "Invalid variable value: " + valueString);
+                }
+            }
+            else
+            {
+                // Invalid command, write to panel
+                throw new InvalidParameterCountException("Invalid number of parameters in command");
+            }
+        }
+
+        private void ExecuteLoop()
+        {
+            List<string> loopCommandsCopy = new List<string>(loopCommands); // Create a copy of loopCommands (needed or invalid operation exception for iterating over original)
+
+            while (EvaluateCondition(loopCondition))
+            {
+                foreach (var cmd in loopCommandsCopy)
+                {
+                    Console.WriteLine($"Executing command: {cmd}"); // Debugging statement
+                    ParseCommand(cmd);
+                }
+                // Re-evaluate the loop condition after executing commands
+                if (!EvaluateCondition(loopCondition))
+                {
+                    break;
+                }
+            }
+        }
+
+        private bool EvaluateCondition(string condition)
+        {
+            if (condition.Contains("==") || condition.Contains("!="))
+            {
+                return equalsHandler.TryHandleEqualsOperator(condition);
+            }
+
+            else if (condition.Contains('<') || condition.Contains('>'))
+            {
+                return comparisonHandler.TryHandleComparisonOperator(condition);
+            }
+
+            else if (condition.Contains('+') || condition.Contains('-'))
+            {
+                return operatorHandler.HandleArithmeticOperators(condition);
+            }
+            return false;
         }
     }   
 }
